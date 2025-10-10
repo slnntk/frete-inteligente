5c64402047ab60d4c722859a443f0a60b21f7076
Danilzs <danzone00@gmail.com>
2025-10-10 11:41:24 -0300

    Primneiro Commit

---
+++ src/main/java/frete_inteligente/com/frete_inteligente/FreteInteligenteApplication.java
@@ -0,0 +1,13 @@
+package frete_inteligente.com.frete_inteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreteInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreteInteligenteApplication.class, args);
+	}
+
+}
